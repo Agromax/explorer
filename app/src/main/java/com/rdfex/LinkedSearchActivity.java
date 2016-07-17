4c4ef60ed55967d1ebd35a1d902cf742694780ca
Anurag Gautam <anurag1812@outlook.com>
2016-07-17 20:26:45 +0530

    Added Triples result view

--- app/src/main/java/com/rdfex/LinkedSearchActivity.java
+++ app/src/main/java/com/rdfex/LinkedSearchActivity.java
@@ -1,5 +1,6 @@
 package com.rdfex;
 
+import android.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +10,17 @@ public class LinkedSearchActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_linked_search);
+
+        decorateActionBar();
+    }
+
+    private void decorateActionBar() {
+        ActionBar actionBar = getActionBar();
+        if (actionBar != null) {
+            actionBar.setHomeButtonEnabled(true);
+        }
+
+        setTitle("Connect Triples");
+
     }
 }

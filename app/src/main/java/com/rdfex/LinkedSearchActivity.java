646d05bedc45b34e93f90664a2c344b8452336f4
Anurag Gautam <anurag1812@outlook.com>
2016-07-18 22:40:56 +0530

    Added LinkedSearchActivity class definition

--- app/src/main/java/com/rdfex/LinkedSearchActivity.java
+++ app/src/main/java/com/rdfex/LinkedSearchActivity.java
@@ -1,8 +1,19 @@
 package com.rdfex;
 
 import android.app.ActionBar;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.rdfex.rdf.Triple;
+import com.rdfex.rdf.TripleLoader;
+
+import java.util.ArrayList;
 
 public class LinkedSearchActivity extends AppCompatActivity {
 
@@ -12,6 +23,99 @@ public class LinkedSearchActivity extends AppCompatActivity {
         setContentView(R.layout.activity_linked_search);
 
         decorateActionBar();
+        setup();
+    }
+
+    private void setup() {
+        ImageButton fromBtn = (ImageButton) findViewById(R.id.trv_search_from);
+        ImageButton toBtn = (ImageButton) findViewById(R.id.trv_search_to);
+
+        if (fromBtn != null) {
+            fromBtn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    handleSearch();
+                }
+            });
+        }
+
+        if (toBtn != null) {
+            toBtn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    handleSearch();
+                }
+            });
+        }
+
+
+    }
+
+    private void handleSearch() {
+        final EditText from = (EditText) findViewById(R.id.trv_from);
+        final EditText to = (EditText) findViewById(R.id.trv_to);
+
+        String fromStr = "";
+        String toStr = "";
+
+        if (from != null) {
+            fromStr = from.getText().toString();
+        }
+
+        if (to != null) {
+            toStr = to.getText().toString();
+        }
+
+        // Perform the actual search
+        doSearch(fromStr.trim(), toStr.trim());
+    }
+
+    private void doSearch(String fromStr, String toStr) {
+        if (fromStr.isEmpty() && !toStr.isEmpty()) {
+            searchSingle(toStr);
+        } else if (!fromStr.isEmpty() && toStr.isEmpty()) {
+            searchSingle(fromStr);
+        } else if (!fromStr.isEmpty() && !toStr.isEmpty()) {
+            connect(fromStr, toStr);
+        }
+    }
+
+    private void connect(String fromStr, String toStr) {
+
+    }
+
+    private void searchSingle(final String q) {
+        System.out.println("Searching for:"+q);
+        new AsyncTask<Void, Void, ArrayList<Triple>>() {
+
+            @Override
+            protected ArrayList<Triple> doInBackground(Void... params) {
+                return TripleLoader.loadTriple(LinkedSearchActivity.this, q);
+            }
+
+            @Override
+            protected void onPostExecute(ArrayList<Triple> triples) {
+                if (triples != null) {
+                    LinearLayout results = (LinearLayout) findViewById(R.id.als_search_results);
+
+                    if (results != null) {
+                        for (Triple t : triples) {
+                            View view = getLayoutInflater().inflate(R.layout.triple_result_view, null);
+                            TextView sub = (TextView) view.findViewById(R.id.trv_sub);
+                            TextView pre = (TextView) view.findViewById(R.id.trv_pre);
+                            TextView obj = (TextView) view.findViewById(R.id.trv_obj);
+
+                            sub.setText(t.getSubject());
+                            pre.setText(t.getPredicate());
+                            obj.setText(t.getObject());
+
+                            results.addView(view);
+                        }
+                    }
+                }
+            }
+        }.execute();
+
     }
 
     private void decorateActionBar() {
